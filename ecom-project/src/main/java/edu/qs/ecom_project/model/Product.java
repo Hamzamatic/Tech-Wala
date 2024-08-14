e5d8cc60aa0eb8bd9fbda95acc245a43b6467a35
Hamza Khan <131784056+Hamzamatic@users.noreply.github.com>
2024-08-14 19:23:03 +0530

    Add files via upload

---
+++ ecom-project/src/main/java/edu/qs/ecom_project/model/Product.java
@@ -0,0 +1,143 @@
+package edu.qs.ecom_project.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int id;
+	
+	private String name;
+	private String description;
+	private BigDecimal price; 
+	private String category;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+	private Date releaseDate;
+	private boolean productAvailable;
+	private int stockQuantity;
+	
+	//IMAGE ATTRIBUTES
+	private String imageName;
+	private String imageType;
+	
+	@Lob 
+	//large object
+	private byte[] imageData;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public boolean isProductAvailable() {
+		return productAvailable;
+	}
+
+	public void setProductAvailable(boolean productAvailable) {
+		this.productAvailable = productAvailable;
+	}
+
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public String getImageType() {
+		return imageType;
+	}
+
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+
+	public byte[] getImageData() {
+		return imageData;
+	}
+
+	public void setImageData(byte[] imageData) {
+		this.imageData = imageData;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", category=" + category + ", releaseDate=" + releaseDate + ", productAvailable=" + productAvailable
+				+ ", stockQuantity=" + stockQuantity + ", imageName=" + imageName + "]";
+	}
+	
+	
+}
+
---
+++ ecom-project/src/main/java/edu/qs/ecom_project/service/ProductService.java
@@ -0,0 +1,48 @@
+package edu.qs.ecom_project.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.qs.ecom_project.model.Product;
+import edu.qs.ecom_project.repo.ProductRepo;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepo repo;
+	
+	public List<Product> getAllProductsDao(){
+		
+		return repo.findAll();
+	}
+	
+	public Product getProductByIdDao(int id) {
+		//Find By Id return you the optional product not the actual product
+		//Product can be null in case there is no matching ID so we should handle that 
+		Optional<Product> opt=repo.findById(id);//It is a container which has a method ispresent return boolean
+		if(opt.isPresent())
+		{
+			return opt.get();
+		}
+		else {
+			return null;
+		}
+		
+	}
+
+	public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+		product.setImageName(imageFile.getOriginalFilename());
+		product.setImageType(imageFile.getContentType());
+		product.setImageData(imageFile.getBytes());
+		return repo.save(product);
+		
+	}
+
+	
+}
